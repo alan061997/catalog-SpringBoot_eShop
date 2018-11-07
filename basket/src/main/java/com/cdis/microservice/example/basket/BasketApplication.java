dec8df62fa63a6ee75522b91a80be9ac3fc82f71
Alberto Alan Zul Rabasa <alan061997@gmail.com>
2018-11-07 12:45:16 -0600

    Implementcacion de Basket con BD de MongoDB. Falta implementacion de RabbitMQ en el servicio de Basket

---
+++ basket/src/main/java/com/cdis/microservice/example/basket/BasketApplication.java
@@ -0,0 +1,16 @@
+package com.cdis.microservice.example.basket;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.data.mongodb.repository.config.EnableMongoRepositories;
+
+@EnableEurekaClient
+@EnableMongoRepositories(basePackages = "com.cdis.microservice.example.basket.repository")
+@SpringBootApplication
+public class BasketApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BasketApplication.class, args);
+    }
+}
